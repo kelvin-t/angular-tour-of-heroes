f3025ad42fe985dadf1b319b19f5545c5ca7d1ed
Tee Kai Yoong <kaiyoong.tee@gmail.com>
2021-09-28 12:27:06 +0800

    Add method 'deleteHero' to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -56,6 +56,16 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
