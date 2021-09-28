1e6c04ee74d4756a31cd5fc80f2c7e5ac9201131
Tee Kai Yoong <kaiyoong.tee@gmail.com>
2021-09-28 10:23:11 +0800

    Inject HttpClient into the constructor of HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 
@@ -25,5 +26,7 @@ export class HeroService {
     return of(hero);
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 }
