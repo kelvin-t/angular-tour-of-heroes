740830f5a7dc06ffbde8253fcb09e9901a72604d
Tee Kai Yoong <kaiyoong.tee@gmail.com>
2021-09-28 12:07:11 +0800

    Fix indentation in hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -60,23 +60,23 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
 
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
-    private handleError<T>(operation = 'operation', result?: T) {
-      return (error: any): Observable<T> => {
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
-        // TODO: send the error to remove logging infrastructure
-        console.error(error); // log to console instead
+      // TODO: send the error to remove logging infrastructure
+      console.error(error); // log to console instead
 
-        // TODO: better job of transforming error for user consumption
-        this.log(`${operation} failed: ${error.message}`);
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
 
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
-      };
-    }
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
